eb50782e80b1490d81b24ce9d2481e733568eb1e
williambilsdon <williambilsdon@gmail.com>
2019-02-21 17:29:55 +0000

    Script to create spectrogram input files
    Python script that turns dataset of mp3s to spectrograms

--- scripts/SortTestData.py
+++ scripts/SortTestData.py
@@ -1,36 +1,67 @@
 
 import os
-import eyed3
-
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, TIT2, TPE1, TRCK, TALB, USLT, error
-
+import csv
+from pydub import AudioSegment as audiosegment
+import matplotlib.pyplot as plt
+from scipy.io import wavfile as wav
 
 source = ("../../../Desktop/fma_small")
+csvFileLocal = ("../../../Desktop/fma_small/tracksordered.csv")
 sourceFolders = os.listdir(source)
-destination = ("../tests/Source MP3s")
+destination = ("../tests/Spectograms")
+holder = "holder.wav"
 
 folder = os.listdir("../tests/Source MP3s")
 
 genres = ["Hip-Hop", "Pop", "Rock", "Jazz", "Classical", "Country"]
 
+            
+
+def specgram(source, genre, destination, filename):
+    file = audiosegment.from_mp3(source)
+
+    #check if mp3 is mono audio
+    if file.channels != 1:
+        file = file.set_channels(1)
+
+    #save new mono file as a wav file
+    file.export(holder, format="wav")
+
+
+    frequency_rate, data = wav.read(holder, 'r')
+    fig = plt.figure(figsize = (19,12))
+    pxx, frequency, bins, im = plt.specgram(x=data, Fs = frequency_rate, cmap = 'plasma', NFFT = 1024)
+    plt.ylim([0, 12000])
+    location = destination + '/' + genre + '/' + filename
+    plt.savefig(location)
+    plt.close()
+
+    os.remove(holder)
+
+
 for root, dirs, files in os.walk(source):
     for filename in files:
         if os.path.splitext(filename)[1] == ".mp3":
-            file = os.path.join(root, filename)
-            #os.chmod(file, S_IWOTH)
-            #mp3 = MP3File(file)
+            trackNum = os.path.splitext(filename)[0]
 
-            #song = eyed3.load(file)
-            song = MP3(file, ID3=ID3)
+            with open(csvFileLocal, 'r') as csvfile:
+                my_content = csv.reader(csvfile, delimiter=',')
+                for row in my_content:
+                    if trackNum in row:
+                        location = os.path.join(root, filename)
+                        print(location)
+                        specgram(location, row[1], destination, trackNum)     
+                        print("specsaved")      
 
-            if song.tags.genre in genres:
-                print("yes")
-                #os.rename(file, destination + mp3.genre)
-                #shutil.copyfile(file, destination + mp3.genre)
+            
+
 
 print("done")
-            
+
+                    
+
+
+
 
 
 
