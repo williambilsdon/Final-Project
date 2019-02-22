3fb410965064dcea6f217bfacf265bcc44fc4b76
williambilsdon <williambilsdon@gmail.com>
2019-02-22 21:12:50 +0000

    Finalised version of dataset bulk tool

--- scripts/SortTestData.py
+++ scripts/SortTestData.py
@@ -1,32 +1,21 @@
-
-@inproceedings{fma_dataset,
-  title = {FMA: A Dataset for Music Analysis},
-  author = {Defferrard, Micha\"el and Benzi, Kirell and Vandergheynst, Pierre and Bresson, Xavier},
-  booktitle = {18th International Society for Music Information Retrieval Conference},
-  year = {2017},
-  url = {https://arxiv.org/abs/1612.01840},
-}
-
 import os
-import csv
 from pydub import AudioSegment as audiosegment
 import matplotlib.pyplot as plt
 from scipy.io import wavfile as wav
 
-source = ("../../../Desktop/fma_medium")
-csvFileLocal = ("../../../Desktop/fma_small/tracksordered.csv")
+source = ("../gtzan")
 sourceFolders = os.listdir(source)
-destination = ("../tests/Spectograms")
+destination = ("../tests")
 holder = "holder.wav"
 
-folder = os.listdir("../tests/Source MP3s")
+
 
 genres = ["Hip-Hop", "Pop", "Rock", "Jazz", "Classical", "Country"]
 
-            
 
-def specgram(source, genre, destination, filename):
-    file = audiosegment.from_mp3(source)
+
+def specgram(trackDest, genre, trackSource):
+    file = audiosegment.from_mp3(trackSource)
 
     #check if mp3 is mono audio
     if file.channels != 1:
@@ -40,8 +29,7 @@ def specgram(source, genre, destination, filename):
     fig = plt.figure(figsize = (19,12))
     pxx, frequency, bins, im = plt.specgram(x=data, Fs = frequency_rate, cmap = 'plasma', NFFT = 1024)
     plt.ylim([0, 8000])
-    location = destination + '/' + genre + '/' + filename
-    plt.savefig(location)
+    plt.savefig(trackDest)
     plt.close()
 
     os.remove(holder)
@@ -52,42 +40,17 @@ jazz = 0
 
 for root, dirs, files in os.walk(source):
     for filename in files:
-        if os.path.splitext(filename)[1] == ".mp3":
-            trackNum = os.path.splitext(filename)[0]
+        trackNum = os.path.splitext(filename)[0]
+        #print(trackNum)
+        genre = trackNum.split(".")
 
-            with open(csvFileLocal, 'r') as csvfile:
-                my_content = csv.reader(csvfile, delimiter=',')
-                for row in my_content:
+        trackDest = destination + '/' + genre[0] + '/' + trackNum + '.png'
+        trackSource = source + '/' + genre[0] + '/' + filename
 
-                    if trackNum in row:
-                        location = os.path.join(root, filename)
-                        print(location)
-                        try:
-                            if row[1] == "Classical" and classical < 390:
-                                specgram(location, row[1], destination, trackNum) 
-                                print("specsaved") 
-                                classical+=1
-                            elif row[1] == "Country" and country < 390:    
-                                specgram(location, row[1], destination, trackNum) 
-                                print("specsaved")
-                                country+=1 
-                            elif row[1] == "Jazz" and jazz < 390:
-                                specgram(location, row[1], destination, trackNum) 
-                                print("specsaved")  
-                                jazz+=1
-                        except:
-                            print("error")
+        #print(trackSource)
 
-            
+        specgram(trackDest, genre, trackSource)
+
 
 
 print("done")
-
-                    
-
-
-
-
-
-
-
