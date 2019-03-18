3986588956b3bf94c21d6639962dbaa1b8dfd1e2
williambilsdon <williambilsdon@gmail.com>
2019-03-18 13:20:20 +0000

    test

---
+++ scripts/loadmodel.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+
+TRAIN_DIR = '../training data'
+TEST_DIR = '../test data'
