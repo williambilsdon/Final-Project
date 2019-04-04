6e8d3d08894290fcffc7557c46825be33ee66ff7
williambilsdon <williambilsdon@gmail.com>
2019-04-04 16:56:32 +0100

    Fixed predictions
    Made some changes that fixed the prediction system, need to perform voting system now

--- iosApp/final_project/lib/main.dart
+++ iosApp/final_project/lib/main.dart
@@ -1,36 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() => runApp(MyApp());
 
-class Dialogue{
-  information(BuildContext context, String title, String description){
-    return showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context){
-        return AlertDialog(
-          title: Text(title),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text(description)
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            FlatButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('Ok'),
-            )
-          ],
-        );
-      },
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
-  Dialogue dialogues = new Dialogue();
 
   @override
   Widget build(BuildContext context) {
@@ -40,86 +14,58 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Music Genre Classifier'),
-        ),
-        body: Center(
-          child: new RawMaterialButton(
-            onPressed: (){
-              dialogues.information(context, 'Title', 'Description');
-              },
-
-            child: new Icon(
-              Icons.mic,
-              color: Colors.white,
-              size: 60.0,
-            ),
-            shape: new CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.red,
-            padding: const EdgeInsets.all(15.0),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-              height: 75,
-              color: Colors.red
-          ),
-        ),
-      ),
+      home: Result()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
+class Result extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text('Music Genre Classifier'),
       ),
       body: Center(
-        child: Column(
+        child: new RawMaterialButton(
+          onPressed: (){
+            showDialog(
+                context: context,
+                builder: (_) => new AlertDialog(
+                  title: new Text("Genre:", textAlign: TextAlign.center),
+                  content: new Text("Example", textAlign: TextAlign.center),
+                  actions: <Widget>[
+                    // usually buttons at the bottom of the dialog
+                    new FlatButton(
+                      child: new Text("Close"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                )
+            );
+          },
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          child: new Icon(
+            Icons.mic,
+            color: Colors.white,
+            size: 60.0,
+          ),
+          shape: new CircleBorder(),
+          elevation: 2.0,
+          fillColor: Colors.red,
+          padding: const EdgeInsets.all(15.0),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+            height: 75,
+            color: Colors.red
+        ),
       ),
     );
   }
 }
+
+
--- scripts/loadmodel.py
+++ scripts/loadmodel.py
@@ -4,9 +4,12 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.callbacks import ModelCheckpoint
 from keras.layers.normalization import BatchNormalization
-from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
 import numpy as np
 import operator
+from pydub import AudioSegment as audiosegment
+from scipy.io import wavfile as wav
+import matplotlib.pyplot as plt
 
 import random
 import cv2
@@ -29,6 +32,7 @@ def isBigger(curNum, nextNum):
 
 model = Sequential()
 model = load_model('130319_1_Model.h5')
+model.load_weights('130319_1_Weights.h5')
 
 countArray = [0,0,0,0,0,0,0,0,0,0]
 
@@ -36,64 +40,104 @@ genreCountDict = dict(zip(genres,countArray))
 
 values = [0,0,0,0,0,0,0,0,0]
 
+imgDims = 256
 
-for root, dirs, files in os.walk('../wholepngs'):
-    for filename in files:
-        predictions = []
-        print(filename)
+file = audiosegment.from_mp3('testing.mp3')
 
-        imageX = 3200
-        numSlices = 10
-        xStride = imageX/numSlices
-        xPos = 0
-        newX = int(xPos) + int(xStride)
-        if filename != '.DS_Store':
-            img = cv2.imread(root + '/' + filename)
-            print('new')
-            for i in range(0,10):
-                slice = img[0:2400, int(xPos):int(newX)]
+    #check if mp3 is mono audio
+if file.channels != 1:
+    file = file.set_channels(1)
 
-                xPos += xStride
-                newX += xStride
-
-                cv2.imwrite(('temp.png'), slice)
-                image = cv2.imread('temp.png')
-                os.remove('temp.png')
-
-                image = cv2.resize(image, (256, 256))
-                image = image.astype("float") / 255.0
-                image = img_to_array(image)
-                image = np.expand_dims(image, axis=0)
-                #proba = model.predict(image)[0]
-                #idx = np.argmax(proba)
-                #prediction = model.predict_classes(image)
-                prediction = model.predict(image)[0]
-                pred = prediction
-
-                predID = 0
-                for i in range(0,9):
-                    values[i] = pred[i]
-                #    print(i)
-                #    if pred[predID] < pred[i]:
-                #        predID = i
-                #    print(predID)
-
-                print(values)
-
-                maxNum = max(values)
-                print(maxNum)
-
-                #for i in range(0, 9):
+    #save new mono file as a wav file
+    file.export('holder.wav', format="wav")
 
 
+    frequency_rate, data = wav.read('holder.wav', 'r')
+    fig, ax = plt.subplots(1)
+    fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+    ax.axis('off')
+    pxx, frequency, bins, im = plt.specgram(x=data, Fs = frequency_rate, cmap = 'plasma', NFFT = 1024)
+    plt.ylim([0, 10000])
+    ax.axis('off')
+    fig.savefig('010030.png', dpi=500, frameon='false')
+    plt.close()
+
+    os.remove('holder.wav')
+
+img = cv2.imread('010030.png')
+for i in range(0,10):
+    slice = img[0:2400, int(xPos):int(newX)]
+    xPos += xStride
+    newX += xStride
+    cv2.imwrite(('temp.png'), slice)
+    #image = cv2.imread('temp.png')
+    #os.remove('temp.png')
+
+    test_image = image.load_img('temp.png', target_size=(imgDims, imgDims))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
 
 
+    result = model.predict(test_image, batch_size=1)
+    print(result)
 
-                predictions.append(predID)
-                print(predictions)
-
-            genreID = np.bincount(predictions).argmax()
-            print(genres[genreID])
+# for root, dirs, files in os.walk('../wholepngs'):
+#     for filename in files:
+#         predictions = []
+#         print(filename)
+#
+#         imageX = 3200
+#         numSlices = 10
+#         xStride = imageX/numSlices
+#         xPos = 0
+#         newX = int(xPos) + int(xStride)
+#         if filename != '.DS_Store':
+#             img = cv2.imread(root + '/' + filename)
+#             print('new')
+#             for i in range(0,10):
+#                 slice = img[0:2400, int(xPos):int(newX)]
+#
+#                 xPos += xStride
+#                 newX += xStride
+#
+#                 cv2.imwrite(('temp.png'), slice)
+#                 image = cv2.imread('temp.png')
+#                 os.remove('temp.png')
+#
+#                 image = cv2.resize(image, (256, 256))
+#                 image = image.astype("float") / 255.0
+#                 image = img_to_array(image)
+#                 image = np.expand_dims(image, axis=0)
+#                 #proba = model.predict(image)[0]
+#                 #idx = np.argmax(proba)
+#                 #prediction = model.predict_classes(image)
+#                 prediction = model.predict(image)[0]
+#                 pred = prediction
+#
+#                 predID = 0
+#                 for i in range(0,9):
+#                     values[i] = pred[i]
+#                 #    print(i)
+#                 #    if pred[predID] < pred[i]:
+#                 #        predID = i
+#                 #    print(predID)
+#
+#                 print(values)
+#
+#                 maxNum = max(values)
+#                 print(maxNum)
+#
+#                 #for i in range(0, 9):
+#
+#
+#
+#
+#
+#                 predictions.append(predID)
+#                 print(predictions)
+#
+#             genreID = np.bincount(predictions).argmax()
+#             print(genres[genreID])
 
 
 
